c1dcb8cf8c87a3d80acfc6e2aa38e29d4ba96284
Miguel <muras.m.m.3@gmail.com>
2021-01-25 13:57:17 +0100

    RESTserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 // Puerto
 // =======================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// =======================
+// Entorno
+// =======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// =======================
+// Base de datos
+// =======================
+
+let urlDB = 'mongodb+srv://TrypleM:wZeJuaPgq3D6b7U@cluster0.ypy7r.mongodb.net/cafe';
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://TrypleM:wZeJuaPgq3D6b7U@cluster0.ypy7r.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser')
@@ -11,37 +15,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('Get usuario');
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
-
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            message: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            usuario: body
-        });
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario/:id', function(req, res) {
-    res.json('Delete usuario');
-});
-
 
 app.listen(port, () => {
     console.log('Escuchando en el puerto ', port);
